50f3c26e41949855df8bd87948fb7f3c07d4cd4a
unknown <shawonahmmed08509@gmail.com>
2023-01-28 23:10:43 +0600

    postgressql connected

--- config/database.js
+++ config/database.js
@@ -1,11 +1,27 @@
-const path = require('path');
+  module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'shawon-sql-db'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', 'sa742941'),
+          schema: env('DATABASE_SCHEMA', 'public'),
+         
+        },
+        debug: false,
+      },
+    });
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
